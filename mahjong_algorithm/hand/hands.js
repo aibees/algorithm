881339b2079b1d54dad8df69526808c0bc9cd864
aibees <aibees1129@naver.com>
2019-02-17 21:44:27 +0900

    combination of mentsu & toitsu

--- mahjong_algorithm/hand/hands.js
+++ mahjong_algorithm/hand/hands.js
@@ -2,8 +2,18 @@ module.exports = tiles => {
     const shuntsu = require('./shuntsu')(tiles)
     const kotsu = require('./Kotsu')(tiles)
     const kantsu = require('./kantsu')(tiles)
+
+    const toitsu = require('./toitsu')(tiles)
     const mentsu = new Array()
-    mentsu.concat(shuntsu)
-    mentsu.concat(kotsu)
-    mentsu.concat(kantsu)
+
+    mentsu.concat(shuntsu, kotsu, kantsu)
+    
+    return result = {
+        mentsu : mentsu,
+        toitsu : toitsu,
+        shuntsu : shuntsu,
+        kotsu : kotsu,
+        kantsu : kantsu
+    }
+    //  hands가 가지고 있는 정보 : shuntsu, kotsu, kantsu, mentsu, toitsu
 }
